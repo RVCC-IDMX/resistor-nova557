49abf5c0368e5d247d4d737a603c547d03b3fd55
nova557 <71886498+nova557@users.noreply.github.com>
2022-04-08 14:00:54 -0400

    Edit Resistor

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 import { getResistorOhms } from './resistor.js';
+
+
+const answer = document.querySelector('.answer');
+
+// console.log(answer);
+
+const color0 = document.querySelector('#color0');
+
+const colors0 = document.querySelectorAll('#color0 .box');
+
+// console.log(colors0);
+
+const colorsArray0 = Array.from(colors0);
+
+console.log(colorsArray0);
+
+colorsArray0.forEach((color) => {
+    const band = color.innerText;
+    color.addEventListener('click', () => {
+
+        console.log(band);
+    });
+})
